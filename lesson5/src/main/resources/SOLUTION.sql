d82dade21d3f9cef99ff78292881aec049090164
1 <test@tut.by>
2024-11-14 19:08:34 +0300

    commit

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 SELECT * FROM payment WHERE amount >=500;
 SELECT * FROM student WHERE birthday <= DATEADD ('YEAR', -20, CURRENT_DATE);
 SELECT * FROM student WHERE birthday >= DATEADD ('YEAR', -20, CURRENT_DATE) AND groupnumber = 10;
-SELECT * FROM student WHERE name = 'Mike' OR groupnumber IN (4,5,6);
+SELECT * FROM student WHERE name = 'Mike McMylor' OR groupnumber IN (4,5,6);
 SELECT * FROM payment WHERE payment_date >= DATEADD('MONTH', -8, '2022-12-31');
 SELECT * FROM student WHERE name LIKE 'A%';
-SELECT * FROM STUDENT WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
+SELECT * FROM STUDENT WHERE (name = 'Roxi Ivakhnov' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
