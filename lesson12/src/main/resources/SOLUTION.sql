e594a109534a62fcbb992503fd1dae7b00eaf2b0
1 <test@tut.by>
2024-11-14 21:51:05 +0300

    commit

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM student WHERE groupnumber >= 4;
-DELETE FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4);
-DELETE FROM paymenttype WHERE name = 'DAILY';
+BEGIN;DELETE FROM mark WHERE student_id IN (SELECT id FROM student WHERE groupnumber >= 4);DELETE FROM payment WHERE student_id IN (SELECT id FROM student WHERE groupnumber >= 4);DELETE FROM student WHERE groupnumber >= 4;COMMIT;
+BEGIN;CREATE TEMPORARY TABLE temp_students AS (SELECT student_id FROM mark WHERE mark < 7);DELETE FROM mark WHERE student_id IN (SELECT student_id FROM temp_students);DELETE FROM PAYMENT WHERE student_id IN (SELECT student_id FROM temp_students);DELETE FROM STUDENT WHERE id IN (SELECT student_id FROM temp_students);DROP TABLE temp_students;COMMIT;
+BEGIN;DELETE FROM payment WHERE type_id IN (SELECT ID FROM paymenttype WHERE name = 'DAILY');DELETE FROM paymenttype WHERE name = 'DAILY';COMMIT;
 DELETE FROM mark WHERE mark < 7;
